ea64ae9c5e5674dcab0d540f8fd897af9b0b5b7a
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-02 11:58:40 -0500

    tested userInput click javascript code

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+var newInput = false;
+
+$("#searchButton").on("click", () => {
+  var searchInput = $("#userInput").val();
+  newInput = true;
+  openWeatherData(searchInput);
+  $("#userInput").val("");
+});
+
+
+$("#searchHistory").on("click", (event) => {
+  if(event.target.matches("button")){
+    var searchInput = event.target.textContent;
+    newInput = false;
+    openWeatherData(searchInput);
+  }
+});
