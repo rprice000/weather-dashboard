0865955db45ca0dc0ca46f28e53e70ccb73130ce
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-03 12:36:55 -0500

    added js function that creates buttons for search history

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,11 @@
-var newInput = false;
-
-$("#searchButton").on("click", () => {
-  var searchInput = $("#userInput").val();
-  newInput = true;
-  openWeatherData(searchInput);
-  $("#userInput").val("");
-});
 
 
-$("#searchHistory").on("click", (event) => {
-  if(event.target.matches("button")){
-    var searchInput = event.target.textContent;
-    newInput = false;
-    openWeatherData(searchInput);
-  }
-});
+function searchListButtons(searchInput) {
+  var cityButton = $("<button>")
+  cityButton.text(searchInput);
+  cityButton.attr("class", " btn btn-block searchListButton");
+  cityButton.attr("cityData", searchInput);
+  $("#searchHistory").prepend(cityButton);
+}
+
+searchListButtons();
