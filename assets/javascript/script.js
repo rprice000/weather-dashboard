feb1ab7848892242c4b22180181f9e7a7c97c5c8
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-10-03 13:02:57 -0500

    added second event listener

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,13 @@
 
 
 
+$("#searchButton").on("click", () => {
+  var searchInput = $("#userInput").val();
+  newInput = true;
+  openWeatherData(searchInput);
+  $("#userInput").val("");
+});
+
 $("#searchHistory").on("click", (event) => {
   if(event.target.matches("button")){
     var searchInput = event.target.textContent;
